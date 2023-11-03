37a75a581690f9b0d8414569ddced662a17eb63b
TimPloetzner <146972208+TimPloetzner@users.noreply.github.com>
2023-11-03 15:16:28 +0100

    new vars

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -8,13 +8,13 @@
 #include "config.h"
 //.bss segment
 int foo;
-
+int var;
 // .data segment
 int data =4711;
-
+int datavar=6969;
 // .rodata segment
 const int rodata =42;
-
+const int rodatavar=420;
 
 auto main(int argc, char **argv) -> int
 {
@@ -27,6 +27,10 @@ auto main(int argc, char **argv) -> int
     fmt::print("Value of variable foo {} address of variable foo {}\n", foo, fmt::ptr(&foo));
     fmt::print("Value of variable data {} address of variable data {}\n", data, fmt::ptr(&data));
     fmt::print("Value of variable rodata {} address of variable rodata {}\n", rodata, fmt::ptr(&rodata));
+
+    fmt::print("Value of variable var {} address of variable var {}\n", var, fmt::ptr(&var));
+    fmt::print("Value of variable datavar {} address of variable datavar {}\n", datavar, fmt::ptr(&datavar));
+    fmt::print("Value of variable rodatavar {} address of variable rodatavar {}\n", rodatavar, fmt::ptr(&rodatavar));
 
     return 0; /* exit gracefully*/
 }
